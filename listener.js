da78f44972c43a49aff18ced9dd1611054f74ec5
ROBIN DU APRC <ROBIN@PRIMES-SERVICES.BE>
2025-05-01 22:06:44 +0200

    a lot of adjustments but done and looks clean

--- listener.js
+++ listener.js
@@ -5,8 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (button && messageBox) {
     button.addEventListener('click', () => {
-      button.textContent = 'at least you remain with a good sound 🎸';
+      button.textContent = 'At least you remain with a good sound 🎸';
       messageBox.style.display = 'block';
+      sound.load();
       sound.play();
     });
   }
